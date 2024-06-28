bc51806aac6a40456f625136f697b868134bd210
Usman Khurshid <usmankq04@gmail.com>
2024-06-28 23:49:00 +0500

    buttons generating dynamically

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'answer_buttons_styling.dart';
+import 'package:second_app/data/questions_answers.dart';
 
 class Questions extends StatefulWidget {
   const Questions({super.key});
@@ -12,19 +13,26 @@ class Questions extends StatefulWidget {
 class _Questions extends State<Questions> {
   @override
   Widget build(context) {
+final currentQuestion=questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question is'),
+           Text(currentQuestion.text,style: TextStyle(color: Colors.white),) ,
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(anstext: 'Answer1', onTap: () {}),
-          AnswerButton(anstext: 'Answer2', onTap: () {}),
-          AnswerButton(anstext: 'Answer3', onTap: () {}),
-          AnswerButton(anstext: 'Answer4', onTap: () {}),
+
+          ...currentQuestion.answers.map((items){
+            return AnswerButton(anstext: items, onTap: (){});
+          }),//we have converted the string list into the list of widgets and used the spread operator to take out the list from a list and using it as simple widgets basically making the buttons dynamically which is very important concept..
+
+          // AnswerButton(anstext: currentQuestion.answers[0], onTap: () {}),
+          // AnswerButton(anstext:currentQuestion.answers[1], onTap: () {}),
+          // AnswerButton(anstext: currentQuestion.answers[2], onTap: () {}),
+          // AnswerButton(anstext: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
